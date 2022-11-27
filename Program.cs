90b6efcd288cdadc0bab0b61a838b05272ca1d43
youpurr <118732359+youpurr@users.noreply.github.com>
2022-11-27 13:06:01 +0700

    task3, task4

--- Program.cs
+++ Program.cs
@@ -1,31 +1,7 @@
-﻿Console.WriteLine("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
- if(num1 > num2)
- {
-     Console.WriteLine("Первое число больше второго");
- }
-else if (num1 < num2)
- {
-Console.WriteLine("Первое число меньше второго");
-}
-else
-{
-    Console.WriteLine("Оба числа равны");
-}
- if(num1 > num3)
- {
-     Console.WriteLine("Первое число больше третьего");
- }
-else if (num1 < num3)
- {
-Console.WriteLine("Первое число меньше третьего");
-}
-else
-{
-    Console.WriteLine("Оба числа равны");
-}
-Console.ReadKey();
+﻿int num = new Random().Next(10,100);
+Console.WriteLine($"Сгенерировано случайное число {num}");
+int x1 = num/10;
+int x2 = num % 10;
+if (x1>x2) Console.WriteLine($"Наибольшая цифра этого числа это {x1}");
+else if (x1<x2) Console.WriteLine($"Наибольшая цифра этого числа это {x2}");
+else Console.WriteLine("цифры одинаковые");
