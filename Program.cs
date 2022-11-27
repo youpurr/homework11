c02f16bb852c1bef9095ce2e7bbb47e9d641dd75
unknown <facturs@mail.com>
2022-11-27 12:27:40 +0700

    homework1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+ if(num1 > num2)
+ {
+     Console.WriteLine("Первое число больше второго");
+ }
+else if (num1 < num2)
+ {
+Console.WriteLine("Первое число меньше второго");
+}
+else
+{
+    Console.WriteLine("Оба числа равны");
+}
+ if(num1 > num3)
+ {
+     Console.WriteLine("Первое число больше третьего");
+ }
+else if (num1 < num3)
+ {
+Console.WriteLine("Первое число меньше третьего");
+}
+else
+{
+    Console.WriteLine("Оба числа равны");
+}
+Console.ReadKey();
